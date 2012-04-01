b397ee33a27758066f88d5b58f773b9518705afa
void <git.email.address@googlemail.com>
2012-04-01 16:17:42 +0200

    Switched to the new revision system

---
+++ LightQuery/Properties/AssemblyInfo.template.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LightQuery")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa687e3e-276a-4a10-bbed-5c92c78c5fc0")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
